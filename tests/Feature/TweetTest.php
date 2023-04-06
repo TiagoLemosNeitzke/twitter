d0983e9ebb35fcf40da5c5238f4f7ecddb7e10d0
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-04-06 17:26:02 -0400

    planing tweet test'

---
+++ tests/Feature/TweetTest.php
@@ -0,0 +1,10 @@
+<?php
+todo('should be able to create a tweet', function(){
+    
+});
+
+todo('body is required');
+
+todo('the tweet body should have a max length of 140 characters');
+
+todo('should show the tweet on the timeline');

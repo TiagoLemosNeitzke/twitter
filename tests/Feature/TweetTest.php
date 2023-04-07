8bf74c77e27587dcc225ec197bf8a159daa1b57c
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-04-07 06:09:07 -0400

    test: be able to create a new tweet

--- tests/Feature/TweetTest.php
+++ tests/Feature/TweetTest.php
@@ -1,7 +1,34 @@
 <?php
-todo('should be able to create a tweet', function(){
+
+use App\Models\User;
+use App\Http\Livewire\Tweet\Create;
+use App\Models\Tweet;
+
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
+
+it('should be able to create a tweet', function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+
     
+    
+    livewire(Create::class)
+        ->set('body', 'This is my first tweet')
+        ->call('tweet')
+        ->assertEmitted('tweet::created');
+
+
+    \Pest\Laravel\assertDatabaseCount('tweets', count: 1);
+
+    expect(Tweet::first())
+        ->body->toBe('This is my first tweet')
+        ->created_by->toBe($user->id);
+
 });
+
+todo('should make sure that only authenticated user can tweet');
 
 todo('body is required');
 

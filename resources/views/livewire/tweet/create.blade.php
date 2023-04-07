175bb8d6d22198945607307d470192c1c9fc5520
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-04-07 08:36:22 -0400

    refactor: add input error component

--- resources/views/livewire/tweet/create.blade.php
+++ resources/views/livewire/tweet/create.blade.php
@@ -1,9 +1,8 @@
-<div>
+<div class="rounded-lg bg-white p-8 shadow-lg">
     <div class="flex flex-col w-60 mb-4">
         <textarea class="border-slate-400 rounded-md" wire:model='body' placeholder="What's happening?"></textarea>
-        @error('body')
-            <span class="text-red-300 font-bold py-2">{{ $message }}</span>
-        @enderror
+
+        <x-input-error messages="{{ $errors->first('body') }}"/>
     </div>
 
     <x-primary-button class="bg-slate-400" wire:click='tweet'>

ea1fbdee5b3bc12c49f6b2b8f31b2af4b5410dec
Tiago Lemos Neitzke <tl.neitzke@hotmail.com>
2023-04-13 14:54:38 -0400

    add padding in the button

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -52,7 +52,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button class="px-4 text-white py-2">{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
